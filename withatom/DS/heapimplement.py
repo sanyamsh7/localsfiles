d1c5901d782e9427a94aae2959a52cbcb50c73c9
sanyamsh7 <sanyam19sharma@gmail.com>
2020-06-30 00:47:00 +0530

    implemented heap_increase_key in heap

--- withatom/DS/heapimplement.py
+++ withatom/DS/heapimplement.py
@@ -1,10 +1,13 @@
 # implementing a HEAP #
 from math import floor
+from sys import exit
 
-def parent(A, i):
+def parent(i):
     """returns the index in A of the parent of i"""
-    if i == 1:
+    if i <= 0:
         return None
+    if i == 2:
+        return 0
     return floor(i/2)
 
 def left(A, i):
@@ -55,5 +58,28 @@ def heap_sort(A):
         max_heapify(A, 0, i)
     return A
 
+def heap_increase_key(A, i, key):
+    """ A still representing a heap where the key of A[i] was increased to key"""
+    if i >= len(A) or i < 0:
+        print("ERROR: NONE node at this index")
+        exit(1)
+
+    if key <= A[i] :
+        print("ERROR: new key must be greater than current key")
+        exit(1)
+
+    A[i] = key
+    # for maintaining heap
+    while i > 0  and A[parent(i)] < A[i]:
+        A[i], A[parent(i)] = A[parent(i)], A[i]
+        i = parent(i)
+
+
+
 array = [23, 5, 56, 23 , 7 ,8, 9, 3, 9]
+# print("sorted output",heap_sort(array))
+build_max_heap(array)
+print(array)
+heap_increase_key(array,4, 100)
+print(array)
 print("sorted output",heap_sort(array))
